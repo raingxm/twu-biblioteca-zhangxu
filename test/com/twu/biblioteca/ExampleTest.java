eb4d52343fd130a75db2ef862733bdac30d61368
zhangxv <xuzhang@thoughtworks.com>
2015-03-02 19:56:56 +0800

    user start application see welcome message.

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -9,5 +13,13 @@ public class ExampleTest {
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testWelcomeMessage() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        BibliotecaApp.main(new String[]{});
+        assertEquals("welcome to use bibliote\n", output.toString());
     }
 }
