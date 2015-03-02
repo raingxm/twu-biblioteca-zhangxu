c37fbc5804b0da35c1fe3fbeea6936cf26c2d155
zhangxv <xuzhang@thoughtworks.com>
2015-03-02 20:32:11 +0800

    refactor code

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,26 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
+    private static List<String> books = new ArrayList<String>();
+
+    public static void addSomeBooksToLib() {
+        books.add("C++ Primer");
+        books.add("Java HeadFirst");
+    }
+
+    public static void showBookList() {
+        for(String book : books) {
+            System.out.println(book);
+        }
+    }
 
     public static void main(String[] args) {
+        addSomeBooksToLib();
         System.out.println("welcome to use biblioteca");
         System.out.println("Book List:");
-        System.out.println("C++ Primer");
-        System.out.println("Java HeadFirst");
+        showBookList();
     }
 }
