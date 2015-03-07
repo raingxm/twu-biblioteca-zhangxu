81cfa5a103e6c662e923bb064a7e7779b19c8668
zhangxv <xuzhang@thoughtworks.com>
2015-03-07 22:45:20 +0800

    change main menu string, use StringUtil instead

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.com.twu.biblioteca.util.StringUtils;
+
 /**
  * Created by xuzhang on 3/4/15.
  */
@@ -12,16 +14,16 @@ public class MainMenu {
     public static final int SHOW_USER_INFO_OPTION = 6;
 
     public static void show() {
-        System.out.println("Main Menu(select one options below, such as 1 or 2):");
-        System.out.println(SHOW_BOOK_LIST_OPTION + ": Display Book List");
-        System.out.println(CHECKOUT_BOOK_OPTION + ": Checkout Book");
-        System.out.println(RETURN_BOOK_OPTION + ": Return Book");
-        System.out.println(SHOW_MOVIE_LIST_OPTION + ": Show Movie List");
-        System.out.println(CHECKOUT_MOVIE_OPTION + ": Checkout Movie(type movie name)");
-        System.out.println(SHOW_USER_INFO_OPTION + ": Show My Profile");
+        System.out.println(StringUtils.MAIN_MENU_OPTION_CHOOSE);
+        System.out.println(SHOW_BOOK_LIST_OPTION + StringUtils.MAIN_MENU_SHOW_BOOK_LIST_OPTION);
+        System.out.println(CHECKOUT_BOOK_OPTION + StringUtils.MAIN_MENU_CHECK_BOOK_OPTION);
+        System.out.println(RETURN_BOOK_OPTION + StringUtils.MAIN_MENU_RETURN_BOOK_OPTION);
+        System.out.println(SHOW_MOVIE_LIST_OPTION + StringUtils.MAIN_MENU_SHOW_MOVIE_LIST_OPTION);
+        System.out.println(CHECKOUT_MOVIE_OPTION + StringUtils.MAIN_MENU_CHECKOUT_MOVIE_OPTION);
+        System.out.println(SHOW_USER_INFO_OPTION + StringUtils.MAIN_MENU_SHOW_USER_INFO_OPTION);
     }
 
     public static void invalidOptionPromptMessage() {
-        System.out.println("Select an invalid option, retry please:");
+        System.out.println(StringUtils.MAIN_MENU_INVALID_INPUT_PROMPT);
     }
 }
---
+++ src/com/twu/biblioteca/com/twu/biblioteca/util/StringUtils.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.com.twu.biblioteca.util;
+
+/**
+ * Created by xuzhang on 3/7/15.
+ */
+public class StringUtils {
+    public static final String MAIN_MENU_OPTION_CHOOSE = "Main Menu(select one " +
+            "options below, such as 1 or 2):";
+    public static final String MAIN_MENU_SHOW_BOOK_LIST_OPTION = ": Display Book List";
+    public static final String MAIN_MENU_CHECK_BOOK_OPTION = ": Checkout Book";
+    public static final String MAIN_MENU_RETURN_BOOK_OPTION = ": Return Book";
+    public static final String MAIN_MENU_SHOW_MOVIE_LIST_OPTION = ": Show Movie List";
+    public static final String MAIN_MENU_CHECKOUT_MOVIE_OPTION = ": Checkout Movie(type movie name)";
+    public static final String MAIN_MENU_SHOW_USER_INFO_OPTION = ": Show My Profile";
+    public static final String MAIN_MENU_INVALID_INPUT_PROMPT = "Select an invalid option, retry please:";
+}
